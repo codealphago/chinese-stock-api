3347663b8268bc427eb2dca6236894d61ed56a20
Roy Liu <liuchuanbo@gmail.com>
2017-04-06 11:02:20 +0800

    update version code

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 NAME = 'chinesestockapi_py2'
-VERSION = '1.0'
+VERSION = '1.1'
 DESCRIPTION = 'Python API to get Chinese stock price for python 2.7'
 LONG_DESCRIPTION = """\
 Library to get Chinese stock price
